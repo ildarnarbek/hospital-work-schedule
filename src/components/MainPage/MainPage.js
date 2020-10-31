1819832619ab3b18f0d320e8a04802665bebfd42
ildarnarbek <narbekovildar13@gmail.com>
2020-10-31 21:40:46 +0400

    simple react component

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 // import { useTheme } from '@material-ui/core/styles';
 
@@ -7,10 +10,35 @@ import useStyles from './styles';
 const MainPage = () => {
   const classes = useStyles();
   // const theme = useTheme();
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+
+  const handleDrop = () => {
+    setCount(0);
+  };
 
   return (
-    <div className={classes.root}>
+    <div className={classes.home}>
       <p>hello Tony</p>
+      <Grid container>
+        <Grid item lg={12} sm={6}>
+          <p>item1</p>
+          <Button variant="contained" color="primary" onClick={handleIncrement}>
+            ++
+          </Button>
+          <Button variant="contained" color="secondary" onClick={handleDrop}>
+            Drop
+          </Button>
+        </Grid>
+        <Grid item lg={12} sm={6} className={classes.item2}>
+          <p> item2 </p>
+          {count}
+        </Grid>
+      </Grid>
     </div>
   );
 };
