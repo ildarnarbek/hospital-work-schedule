5f67dd94bb6251f8d5c018410a93ac36e9ff55ee
ildarnarbek <narbekovildar13@gmail.com>
2020-10-31 20:45:44 +0400

    cleared code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { ThemeProvider } from '@material-ui/core/styles';
 
-import logo from './logo.svg';
 import './App.css';
 import theme from './theme';
 import MainPage from './components/MainPage';
@@ -10,17 +9,6 @@ import MainPage from './components/MainPage';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
       <MainPage />
     </ThemeProvider>
   );
--- src/components/MainPage/styles.js
+++ src/components/MainPage/styles.js
@@ -1,9 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-  },
+  root: {},
 });
 
 export default useStyles;
